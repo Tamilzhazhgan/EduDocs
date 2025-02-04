b3912797f1783bb8ea1ddcf1d397ad41b160e4a9
Tamilzhazhgan <130178206+Tamilzhazhgan@users.noreply.github.com>
2025-02-04 23:13:33 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,9 +24,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+    header: "Roboto Slab",
+    body: "Roboto Slab",
+    code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
